47a130c1d6c80ffcfaf1f09e03067f64a488a667
andrei-L1 <adr.lonsania@gmail.com>
2026-02-21 09:51:37 +0800

    fix: add missing User model import in LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 class LoginController extends Controller
 {
